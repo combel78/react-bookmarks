d47bcc7fce26242c416523f13fbfc72c807a7285
dirkmittmann <dirkmittmann@gmail.com>
2020-04-22 12:22:24 +0200

    03_BookmarkListSortierung

--- src/Bookmarks/BookmarkList.tsx
+++ src/Bookmarks/BookmarkList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Bookmark from './Bookmark';
-import { Grid } from '@material-ui/core';
+import { Grid, Select, MenuItem } from '@material-ui/core';
 import BookmarkService from './BookmarkService';
 
 const BookmarkList: React.FunctionComponent = () => {
@@ -8,10 +8,27 @@ const BookmarkList: React.FunctionComponent = () => {
     const bookmarkService = new BookmarkService();
 
     const [bookmarks, setBookmarks] = React.useState(bookmarkService.getBookmarks());
+    const [bookmarkSort, setBookmarkSort] = React.useState(0);
+
+    const handleBookmarkSort = (event: React.ChangeEvent<{value: unknown}>) => {
+        setBookmarkSort(event.target.value as number);
+        setBookmarks(bookmarkService.getBookmarksSort(event.target.value as number));
+    }
 
     return (
         <React.Fragment>
             <Grid container spacing={4}>
+                <Grid item xs={10} />
+                <Grid item xs={2}>
+                    <Grid container justify="flex-end">
+                        <Select id="selectBookmarkSort" value={bookmarkSort} onChange={handleBookmarkSort}>
+                            <MenuItem value={0}>Reihenfolge</MenuItem>
+                            <MenuItem value={1}>Reihenfolge, umgekehrt</MenuItem>
+                            <MenuItem value={2}>alphabetisch</MenuItem>
+                            <MenuItem value={3}>alphabetisch, umgekehrt</MenuItem>
+                        </Select>
+                    </Grid>
+                </Grid>
                 <Grid item xs={12}>
                     {
                         bookmarks.map((bookmark: BookmarkType) => {
